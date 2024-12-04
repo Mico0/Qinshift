2ae1a7021584d6e61958bee31eca8aa184eff892
unknown <milan.ognjanoski@gmail.com>
2024-12-04 11:19:23 +0100

    Finalization of Homework_7

--- 0_Homeworks/CSS_homeworks/Homework_7/scripts/script.js
+++ 0_Homeworks/CSS_homeworks/Homework_7/scripts/script.js
@@ -1,5 +1,5 @@
 function hamMenu() {
-  let menu = document.getElementById("menu_mobile");
+  var menu = document.getElementById("menu_mobile");
   if (menu.style.display === "block") {
     menu.style.display = "none";
   } else {
