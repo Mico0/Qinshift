ad2886a56de8cc240a2272ad310fc8e839522c3e
unknown <milan.ognjanoski@gmail.com>
2024-12-11 14:41:20 +0100

    Functions - Homework_10

--- 0_Homeworks/JavaScript _homeworks/Homework_10/scripts/script.js
+++ 0_Homeworks/JavaScript _homeworks/Homework_10/scripts/script.js
@@ -61,3 +61,88 @@ console.log(compare("ABC", "DE"));
 console.log(compare("hello", "sedc"));
 console.log(compare("hello", "sedca"));
 console.log("=============\n\n");
+
+function isPlural(word) {
+  for (let i = 0; i < word.length; i++) {
+    if (word[word.length - 1] === "s") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+console.log(isPlural("changes"));
+console.log(isPlural("change"));
+console.log(isPlural("dudes"));
+console.log(isPlural("magic"));
+console.log("=============\n\n");
+
+function match(str1, str2) {
+  if (str1.toLowerCase() === str2.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(match("hello", "hELLo"));
+console.log(match("motive", "emotive"));
+console.log(match("venom", "VENOM"));
+console.log(match("mask", "mAskinG"));
+console.log("=============\n\n");
+
+function monthName(number) {
+  switch (number) {
+    case 1:
+      return "January";
+    case 2:
+      return "February";
+    case 3:
+      return "March";
+    case 4:
+      return "April";
+    case 5:
+      return "May";
+    case 6:
+      return "June";
+    case 7:
+      return "July";
+    case 8:
+      return "August";
+    case 9:
+      return "September";
+    case 10:
+      return "October";
+    case 11:
+      return "November";
+    case 12:
+      return "December";
+  }
+}
+
+console.log(monthName(3));
+console.log(monthName(12));
+console.log(monthName(6));
+console.log("============\n\n");
+
+function isTruthy(value) {
+  if (
+    value === false ||
+    value === undefined ||
+    value === 0 ||
+    value === null ||
+    value === "" ||
+    Number.isNaN(value)
+  ) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+console.log(isTruthy(NaN));
+console.log(isTruthy(0));
+console.log(isTruthy(""));
+console.log(isTruthy("false"));
+console.log("============\n\n");
