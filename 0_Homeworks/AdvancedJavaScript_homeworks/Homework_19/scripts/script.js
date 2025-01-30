9cbc37eb8f87d0cf480778cbbb87276584f10329
unknown <milan.ognjanoski@gmail.com>
2025-01-30 16:03:16 +0100

    Committing Homework 19

---
+++ 0_Homeworks/AdvancedJavaScript_homeworks/Homework_19/scripts/script.js
@@ -0,0 +1,53 @@
+let path = "./homework/students.json";
+
+let getJSON = (path) => {
+  fetch(path)
+    .then((path) => path.json())
+    .then((data) => {
+      let avrgHigherThan3 = data.filter((x) => x.averageGrade > 3);
+
+      console.log("Students with average grade higher than 3: ");
+      console.log(avrgHigherThan3);
+
+      let allFemaleWithAvrg5 = data
+        .filter((x) => x.averageGrade === 5)
+        .map((x) => x.firstName);
+
+      console.log("All female student names with an average grade of 5");
+      console.log(allFemaleWithAvrg5);
+
+      let allMaleInSkopje = data
+        .filter((x) => x.age > 18 && x.city === "Skopje")
+        .map((x) => `${x.firstName} ${x.lastName}`);
+
+      console.log(
+        "All male student full names who live in Skopje and are over 18 years old"
+      );
+      console.log(allMaleInSkopje);
+
+      let avrgGradesOfAllFemale = data
+        .filter((x) => x.gender === "Female" && x.age >= 24)
+        .map((x) => x.averageGrade);
+
+      console.log(
+        "The average grades of all female students over the age of 24"
+      );
+      console.log(avrgGradesOfAllFemale);
+
+      let allMaleStartingB = data.filter(
+        (x) =>
+          x.firstName.startsWith("B") &&
+          x.averageGrade > 2 &&
+          x.gender === "Male"
+      );
+
+      console.log(
+        "All male students with a name starting with B and average grade over 2"
+      );
+      console.log(allMaleStartingB);
+
+      //   console.log(data);
+    });
+};
+
+getJSON(path);
