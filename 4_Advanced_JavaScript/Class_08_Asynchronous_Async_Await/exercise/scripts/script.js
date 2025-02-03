b63b796681d90c4b9b5565eab0f33df6548c832e
Mico0 <milan.ognjanoski@gmail.com>
2025-02-03 22:45:55 +0100

    Added TODO

--- 4_Advanced_JavaScript/Class_08_Asynchronous_Async_Await/exercise/scripts/script.js
+++ 4_Advanced_JavaScript/Class_08_Asynchronous_Async_Await/exercise/scripts/script.js
@@ -3,6 +3,7 @@ let url = "https://restcountries.com/v2/alpha/mkd";
 async function getCountriesAsync(url) {
   let response = await fetch(url);
   let data = await response.json();
+  console.log(data);
   let neighbours = [];
   for (let border of data.borders) {
     let borderFetch = border;
@@ -12,7 +13,20 @@ async function getCountriesAsync(url) {
     let dataFetch = await responseFetch.json();
     neighbours[dataFetch.name] = dataFetch;
   }
+
+  console.log(`Neighbours:`);
   console.log(neighbours);
 }
 
 getCountriesAsync(url);
+
+//TODO: Ask why this does not work
+
+/*
+  console.log(`Country: ${data.name} \n`);
+  console.log(data + "\n");
+
+
+  console.log(`Country: ${data.name} \n ${data}`);
+
+*/
