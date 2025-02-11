7c08b221812b020bd5dffc0d413d2b45bd2fbdb0
Mico0 <milan.ognjanoski@gmail.com>
2025-02-11 22:09:01 +0100

    Resolved with async

--- 0_Homeworks/AdvancedJavaScript_homeworks/Homework_20/bonus_exerciseAsync/scripts/script.js
+++ 0_Homeworks/AdvancedJavaScript_homeworks/Homework_20/bonus_exerciseAsync/scripts/script.js
@@ -34,15 +34,22 @@ function toggleDisplay(showDiv) {
 }
 
 register.addEventListener("click", () => toggleDisplay("register"));
-logout.addEventListener("click", () => toggleDisplay("login"));
+logout.addEventListener("click", () => {
+  toggleDisplay("login");
+  let header = document.getElementsByTagName("h2")[0];
+  if (header) {
+    header.remove();
+  }
+});
 
 function checkCredentials(username, password) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       try {
+        // debugger;
         let isFound = false;
         for (let user of arrayOfUsers) {
-          if (username === user.username && password === user.password) {
+          if (username === user.email && password === user.password) {
             isFound = true;
             resolve(`Welcome back ${user.firstName} ${user.lastName}`);
             break;
@@ -57,3 +64,62 @@ function checkCredentials(username, password) {
     }, 2000);
   });
 }
+
+async function validateCredentials() {
+  let username = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  try {
+    let response = await checkCredentials(username, password);
+    toggleDisplay("logout");
+    let header = document.createElement("h2");
+    header.innerText = response;
+    logoutDiv.prepend(header);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function registerUser() {
+  const registerFirstName = document.getElementById("registerFirstName");
+  const registerLastName = document.getElementById("registerLastName");
+  const registerEmail = document.getElementById("registerEmail");
+  const registerPassword = document.getElementById("registerPassword");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (
+        registerFirstName.value &&
+        registerLastName.value &&
+        registerEmail.value &&
+        registerPassword.value
+      ) {
+        resolve(
+          arrayOfUsers.push(
+            new User(
+              registerEmail.value,
+              registerFirstName.value,
+              registerLastName.value,
+              registerPassword.value
+            )
+          )
+        );
+      } else {
+        reject("All fields are required");
+      }
+    }, 1000);
+  });
+}
+
+async function handleRegister() {
+  try {
+    let promise = await registerUser();
+    console.log("Registration successfull! Please log in");
+    toggleDisplay("register");
+    toggleDisplay("login");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+registerBtn.addEventListener("click", handleRegister);
+
+login.addEventListener("click", validateCredentials);
