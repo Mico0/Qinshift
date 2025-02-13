93cfd524da5c1cd4e8d1804aa073d42a3ef83dce
unknown <milan.ognjanoski@gmail.com>
2025-02-13 15:42:28 +0100

    Extra homework - Bands init

---
+++ 0_Homeworks/AdvancedJavaScript_homeworks/Homework_N_ExtraHomework/bands/scripts/script.js
@@ -0,0 +1,40 @@
+const url =
+  "https://raw.githubusercontent.com/Mico0/Qinshift/refs/heads/main/0_Homeworks/AdvancedJavaScript_homeworks/Homework_N_ExtraHomework/bands/bands.json";
+
+async function fetchData(url) {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData(url);
+
+async function generateTable() {
+  const bandTable = document.getElementById("bandTable");
+  const tbody = bandTable.getElementsByTagName("tbody")[0];
+  let data = await fetchData(url);
+  let html = "";
+  for (let i = 0; i < 10; i++) {
+    html += `
+        <tr>
+            <td>${i + 1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].active}</td>
+            <td>${data[i].tags.toString()}</td>
+            <td>
+             ${data[i].members.forEach((element) => {
+               console.log(element.name);
+               return element.name;
+             })}
+            </td>
+             <td>${data[i].albums.length}</td>
+        </tr>
+    `;
+  }
+  tbody.innerHTML = html;
+}
+generateTable();
