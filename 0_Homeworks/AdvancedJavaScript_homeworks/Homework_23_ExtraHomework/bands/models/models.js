852008e26cada6fd9601932e666511e025ca2f9b
unknown <milan.ognjanoski@gmail.com>
2025-02-24 15:30:59 +0100

    Modular JS tryout

---
+++ 0_Homeworks/AdvancedJavaScript_homeworks/Homework_23_ExtraHomework/bands/models/models.js
@@ -0,0 +1,15 @@
+export default class Band {
+  constructor(name, isActive, tags, bandMembers, albumsNum) {
+    this.name = name;
+    this.isActive = isActive;
+    this.tags = tags;
+    this.bandMembers = bandMembers;
+    this.albumsNum = albumsNum;
+  }
+}
+
+export class Tags {
+  constructor(tagName) {
+    this.tagName = tagName;
+  }
+}
