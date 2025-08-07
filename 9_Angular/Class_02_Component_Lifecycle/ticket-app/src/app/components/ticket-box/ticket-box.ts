2873249be2a6e364370ed01efa7d30c64f4b629d
unknown <milan.ognjanoski@gmail.com>
2025-08-07 10:00:34 +0200

    Added finished lifecycle code, added presentations and readmes

--- 9_Angular/Class_02_Component_Lifecycle/ticket-app/src/app/components/ticket-box/ticket-box.ts
+++ 9_Angular/Class_02_Component_Lifecycle/ticket-app/src/app/components/ticket-box/ticket-box.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Ticket } from '../../models/ticket.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-ticket-box',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './ticket-box.html',
   styleUrl: './ticket-box.scss',
 })
-export class TicketBox {}
+export class TicketBox {
+  ticketOutput = output<Ticket>();
+
+  ticket = input.required<Ticket>();
+
+  onTicketClick() {
+    this.ticketOutput.emit(this.ticket());
+  }
+}
