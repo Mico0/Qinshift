2088c0d473a6f35803de607313ee77ff8e80e6cb
unknown <milan.ognjanoski@gmail.com>
2025-08-13 09:44:37 +0200

    Started working on module homework

---
+++ 0_Homeworks/Angular_homeworks/Homework_44_Angular_basics/homework-app-module/src/app/feature/tasks/tasks-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksPage } from './tasks-page/tasks-page';
+
+
+
+@NgModule({
+  declarations: [
+    TasksPage
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TasksModule { }
---
+++ 0_Homeworks/Angular_homeworks/Homework_44_Angular_basics/homework-app-module/src/app/feature/tasks/tasks-page/tasks-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-page',
+  standalone: false,
+  templateUrl: './tasks-page.html',
+  styleUrl: './tasks-page.scss'
+})
+export class TasksPage {
+
+}
